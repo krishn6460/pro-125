21dcd114851a8b4ec752f00e49b14c03bceb2678
krishn6460 <74172120+krishn6460@users.noreply.github.com>
2023-01-14 14:39:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from classifier import  get_alphabet
+
+App = Flask(__name__)
+
+
+@app.route("/predict-alphabet", methods=["POST"])
+
+def predict_data():
+  image = cv2.imdecode(np.fromstring(request.files.get("alphabet").read(), np.uint8), cv2.IMREAD_UNCHANGED)
+  image = request.files.get("alphabet")
+  alphabet = get_alphabet(image)
+  return jsonify({
+    "alphabet_predicted": alphabet
+  }), 200
+
+
+if __name__ == "__main__":
+  App.run(debug=True)
